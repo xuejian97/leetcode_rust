cf19ae4808b445f5012c624c0dbe6cb5d6ebd203
qixuejian <xuejian97@outlook.com>
2022-08-08 19:36:23 +0800

    7. Reverse Integer

--- src/bin/_7_reverse-integer.rs
+++ src/bin/_7_reverse-integer.rs
@@ -1,3 +1,33 @@
+/*
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+ 
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+
+来源：力扣（LeetCode）
+链接：https://leetcode.cn/problems/reverse-integer
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
 impl Solution {
     pub fn reverse(x: i32) -> i32 {
         let i = if x > 0 { 1 } else { -1 };
