a01825957cd87e6a80318663707edb430c1ed306
qixuejian <xuejian97@outlook.com>
2022-08-12 09:18:08 +0800

    fix(test): 1282. Group the People Given the Group Size They Belong To

--- src/bin/_1282_group-the-people-given-the-group-size-they-belong-to.rs
+++ src/bin/_1282_group-the-people-given-the-group-size-they-belong-to.rs
@@ -61,28 +61,26 @@ impl Solution {
         map.iter()
             .flat_map(|(_, v)| v.clone())
             .collect::<Vec<Vec<i32>>>()
-
     }
 }
 
 struct Solution;
 
 fn main() {
-    assert_eq!(
-        Solution::group_the_people(vec![3, 3, 3, 3, 3, 1, 3]),
-        vec![
-            vec![5],
-            vec![0, 1, 2],
-            vec![3, 4, 6],
-        ]
-    );
+    println!("{:?}", Solution::group_the_people(vec![3, 3, 3, 3, 3, 1, 3]));
+    println!("{:?}", vec![
+        vec![5],
+        vec![0, 1, 2],
+        vec![3, 4, 6],
+    ]);
 
-    assert_eq!(
-        Solution::group_the_people(vec![2, 1, 3, 3, 3, 2]),
-        vec![
-            vec![1],
-            vec![0, 5],
-            vec![2, 3, 4],
-        ]
-    );
+    println!("=========================");
+
+    println!("{:?}", Solution::group_the_people(vec![2, 1, 3, 3, 3, 2]));
+    println!("{:?}", vec![
+        vec![1],
+        vec![0, 5],
+        vec![2, 3, 4],
+    ]);
+
 }
