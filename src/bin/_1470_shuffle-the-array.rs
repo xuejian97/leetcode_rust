8b5b60562822b27fe5cb5bbd8c2f97baf7250801
qixuejian <xuejian97@outlook.com>
2022-08-29 10:43:07 +0800

    1470. Shuffle the Array

---
+++ src/bin/_1470_shuffle-the-array.rs
@@ -0,0 +1,29 @@
+impl Solution {
+    pub fn shuffle(nums: Vec<i32>, n: i32) -> Vec<i32> {
+        let mut res = vec![];
+        for i in 0..n as usize {
+            res.push(nums[i]);
+            res.push(nums[i + n as usize]);
+        }
+        res
+    }
+}
+
+struct Solution;
+
+fn main() {
+    assert_eq!(
+        Solution::shuffle(vec![2, 5, 1, 3, 4, 7], 3),
+        vec![2, 3, 5, 4, 1, 7]
+    );
+
+    assert_eq!(
+        Solution::shuffle(vec![1, 2, 3, 4, 4, 3, 2, 1], 4),
+        vec![1, 4, 2, 3, 3, 2, 4, 1]
+    );
+
+    assert_eq!(
+        Solution::shuffle(vec![1, 1, 2, 2], 2),
+        vec![1, 2, 1, 2]
+    );
+}
