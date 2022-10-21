a161e9f7fd50c8b1aa0ad60362e30db20d9abbce
qixuejian <xuejian97@outlook.com>
2022-10-21 10:35:00 +0800

    901. Online Stock Span

---
+++ src/bin/_901_online-stock-span.rs
@@ -0,0 +1,75 @@
+/*
+Design an algorithm that collects daily price quotes for some stock and returns the span of that stock's price for the current day.
+
+The span of the stock's price today is defined as the maximum number of consecutive days (starting from today and going backward) for which the stock price was less than or equal to today's price.
+
+For example, if the price of a stock over the next 7 days were [100,80,60,70,60,75,85], then the stock spans would be [1,1,1,2,1,4,6].
+Implement the StockSpanner class:
+
+StockSpanner() Initializes the object of the class.
+int next(int price) Returns the span of the stock's price given that today's price is price.
+
+
+Example 1:
+
+Input
+["StockSpanner", "next", "next", "next", "next", "next", "next", "next"]
+[[], [100], [80], [60], [70], [60], [75], [85]]
+Output
+[null, 1, 1, 1, 2, 1, 4, 6]
+
+Explanation
+StockSpanner stockSpanner = new StockSpanner();
+stockSpanner.next(100); // return 1
+stockSpanner.next(80);  // return 1
+stockSpanner.next(60);  // return 1
+stockSpanner.next(70);  // return 2
+stockSpanner.next(60);  // return 1
+stockSpanner.next(75);  // return 4, because the last 4 prices (including today's price of 75) were less than or equal to today's price.
+stockSpanner.next(85);  // return 6
+
+
+Constraints:
+
+1 <= price <= 105
+At most 104 calls will be made to next.
+ */
+
+struct StockSpanner {
+    stocks: Vec<i32>,
+    spans: Vec<i32>,
+}
+
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl StockSpanner {
+    fn new() -> Self {
+        StockSpanner { stocks: vec![], spans: vec![] }
+    }
+
+    fn next(&mut self, price: i32) -> i32 {
+        self.stocks.push(price);
+        let mut span = 1;
+        let mut i = self.stocks.len() as i32 - 2;
+        while i >= 0 {
+            if self.stocks[i as usize] > price {
+                break;
+            }
+            span += self.spans[i as usize];
+            i -= self.spans[i as usize];
+        }
+        self.spans.push(span);
+        span
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * let obj = StockSpanner::new();
+ * let ret_1: i32 = obj.next(price);
+ */
+
+fn main() {}
